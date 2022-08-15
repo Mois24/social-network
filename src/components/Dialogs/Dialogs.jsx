5dbc558ae5327d8fac3b28214b4df7abfb1eaaec
Mois24 <artyommois024@gmail.com>
2022-08-15 22:56:19 +0300

    update component dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css";
 
-const Dialogs = () => {
-    return (
-        <div className={classes.item}> 
-            Dialogs
-        </div>
-    );
-}
+const Dialogs = (props) => {
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.dialog + ' ' + classes.active}><NavLink to="/dialogs/1">Darya</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/2">Viktor</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/3">Oksana</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/4">Anton</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/5">Andrey</NavLink></div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi, How are you?</div>
+        <div className={classes.message}>Ooh man, it's real nice!</div>
+        <div className={classes.message}>Is your music channel "GSM"?</div>
+      </div>
+    </div>
+  );
+};
 export default Dialogs;
